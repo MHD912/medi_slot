4c6eab32ec216e0fa7a88ee2307bc7b0182db039
Mohammad Hussein Hamed <hussein.912.workshop@gmail.com>
2024-12-27 21:38:16 +0300

    Added constants in `AppAssets`, `AppRoutes`, and `AppStrings`.

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,38 @@
+class AppAssets {
+  static final scalable = _ScalableAssets();
+  static final raster = _RasterAssets();
+  static final gif = _GifAssets();
+}
+
+class _ScalableAssets {
+  final appLogo = "assets/images/app_logo.svg";
+  final onboarding1 = "assets/images/undraw_doctor_kw-5-l 2.svg";
+  final onboarding2 = "assets/images/undraw_medicine_b-1-ol 2.svg";
+  final bell = "assets/icons/bell.svg";
+  final calendar = "assets/icons/calendar.svg";
+  final facebook = "assets/icons/facebook.svg";
+  final favoriteFilled = "assets/icons/favorite-filled.svg";
+  final favorite = "assets/icons/favorite.svg";
+  final googleChrome = "assets/icons/google-chrome.svg";
+  final home = "assets/icons/home.svg";
+  final leaveDoor = "assets/icons/leave-door.svg";
+  final search = "assets/icons/search.svg";
+  final star = "assets/icons/star.svg";
+  final timeClock = "assets/icons/time-clock.svg";
+  final visibilityOff = "assets/icons/visibility_off.svg";
+  final visibilityOn = "assets/icons/visibility_on.svg";
+  final twitter = "assets/icons/x-twitter.svg";
+}
+
+class _RasterAssets {
+  final person1 = "assets/images/pfp_1.jpg";
+  final person2 = "assets/images/pfp_2.jpg";
+  final person3 = "assets/images/pfp_3.jpg";
+  final profilePicture = "assets/images/pfp_placeholder.jpg";
+}
+
+class _GifAssets {
+  final brain = "assets/animations/animation-brain.gif";
+  final heart = "assets/animations/animation-heart.gif";
+  final tooth = "assets/animations/animation-tooth.gif";
+}
---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,12 @@
+enum AppRoutes {
+  splashScreen,
+  onboarding,
+  welcome,
+  login,
+  signup,
+  forgotPassword,
+  verification,
+  changePassword,
+  home,
+  profile,
+}
