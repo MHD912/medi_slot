e335f7b9727746bfd9c7fd9463c9a60c782bdd50
Mohammad Hussein Hamed <hussein.912.workshop@gmail.com>
2024-12-29 22:18:21 +0300

    Added `welcome_page` UI and logic.

--- lib/features/onboarding/welcome_page/page/welcome_page.dart
+++ lib/features/onboarding/welcome_page/page/welcome_page.dart
@@ -1,10 +1,58 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:medi_slot/core/constants/app_routes.dart';
+import 'package:medi_slot/core/constants/app_strings.dart';
+import 'package:medi_slot/core/constants/app_styles.dart';
+import 'package:medi_slot/shared/widgets/custom_material_button.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Center(
+              child: Text(
+                AppStrings.welcome,
+                style: AppStyles.f40w700OnSurface(context),
+              ).tr(),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              spacing: 24.h,
+              children: [
+                _signupButtonWidget(context),
+                _loginButtonWidget(context),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  CustomMaterialButton _signupButtonWidget(BuildContext context) {
+    return CustomMaterialButton(
+      label: AppStrings.signup,
+      onPressed: () => context.pushNamed(AppRoutes.signup.name),
+    );
+  }
+
+  CustomMaterialButton _loginButtonWidget(BuildContext context) {
+    return CustomMaterialButton(
+      label: AppStrings.login,
+      onPressed: () => context.pushNamed(AppRoutes.login.name),
+      color: Theme.of(context).colorScheme.primaryContainer,
+      labelStyle: AppStyles.f16w700(context).copyWith(
+        color: Theme.of(context).colorScheme.primary,
+      ),
+    );
   }
 }
